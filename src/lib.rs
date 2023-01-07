708e94fa6688ebca07d41a468bd5a47896df15e3
Leo Alt <leo@ethereum.org>
2023-01-07 14:26:44 +0100

    fmt && clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,31 @@
 #![allow(non_snake_case)]
 mod constants;
-use zokrates_field::Pow;
 use crate::constants::Constants;
-pub use zokrates_field::{Field, Bn128Field};
+use zokrates_field::Pow;
+pub use zokrates_field::{Bn128Field, Field};
 
-
-fn ark(mut state: Vec<Bn128Field>, c: &Vec<Bn128Field>, it: u32) -> Vec<Bn128Field> {
+fn ark(mut state: Vec<Bn128Field>, c: &[Bn128Field], it: u32) -> Vec<Bn128Field> {
     let N = state.len() as u32;
     for i in 0..N {
         state[i as usize] = state[i as usize].clone() + c[it as usize + i as usize].clone();
-    };
-    return state
+    }
+    state
 }
 
 fn sbox(mut state: Vec<Bn128Field>, f: u32, p: u32, r: u32) -> Vec<Bn128Field> {
     let N = state.len() as u32;
     state[0] = Pow::pow(state[0].clone(), 5);
     for i in 1..N {
-        state[i as usize] = if (r < f/2) || (r >= f/2 + p) {
+        state[i as usize] = if (r < f / 2) || (r >= f / 2 + p) {
             Pow::pow(state[i as usize].clone(), 5)
         } else {
             state[i as usize].clone()
         }
     }
-    return state
+    state
 }
 
-fn mix(state: Vec<Bn128Field>, m: &Vec<Vec<Bn128Field>>) -> Vec<Bn128Field> {
+fn mix(state: Vec<Bn128Field>, m: &[Vec<Bn128Field>]) -> Vec<Bn128Field> {
     let N = state.len() as u32;
     let mut out: Vec<Bn128Field> = vec![Field::try_from_str("0", 10).unwrap(); N as usize];
     for i in 0..N {
@@ -36,7 +35,7 @@ fn mix(state: Vec<Bn128Field>, m: &Vec<Vec<Bn128Field>>) -> Vec<Bn128Field> {
         }
         out[i as usize] = acc;
     }
-    return out
+    out
 }
 
 pub fn hash_BN_128(input: Vec<Bn128Field>) -> Bn128Field {
@@ -46,31 +45,28 @@ pub fn hash_BN_128(input: Vec<Bn128Field>) -> Bn128Field {
         println!("through an error");
     }
 
-    let t:u32 = N + 1;
+    let t: u32 = N + 1;
     let rounds_p: Vec<u32> = vec![56, 57, 56, 60, 60, 63, 64, 63];
 
-    let f:u32 = 8;
-    let p:u32 = rounds_p[t as usize - 2];
+    let f: u32 = 8;
+    let p: u32 = rounds_p[t as usize - 2];
 
-    let c = &con.POSEIDON_C[t as usize -2];
-    let m = &con.POSEIDON_M[t as usize -2];
+    let c = &con.POSEIDON_C[t as usize - 2];
+    let m = &con.POSEIDON_M[t as usize - 2];
 
     let mut state: Vec<Bn128Field> = vec![Field::try_from_str("0", 10).unwrap(); t as usize];
     for i in 1..t {
         state[i as usize] = input[i as usize - 1].clone();
     }
 
-    for r in 0..f+p {
+    for r in 0..f + p {
         state = ark(state, c, r * t);
         state = sbox(state, f, p, r);
         state = mix(state, m);
     }
 
-    return state[0].clone()
+    state[0].clone()
 }
-
-
-
 
 #[cfg(test)]
 mod tests {
@@ -79,23 +75,31 @@ mod tests {
 
     #[test]
     fn poseidon_1() {
-        let input: Vec<Bn128Field> = vec![
-            Field::try_from_str("1", 10).unwrap(),
-        ]; 
+        let input: Vec<Bn128Field> = vec![Field::try_from_str("1", 10).unwrap()];
 
         let hash: Bn128Field = hash_BN_128(input);
         println!("{}", hash);
 
-        assert!(hash == Field::try_from_str("18586133768512220936620570745912940619677854269274689475585506675881198879027", 10).unwrap());
+        assert!(
+            hash == Field::try_from_str(
+                "18586133768512220936620570745912940619677854269274689475585506675881198879027",
+                10
+            )
+            .unwrap()
+        );
 
-        let input: Vec<Bn128Field> = vec![
-            Field::try_from_str("42", 10).unwrap(),
-        ]; 
+        let input: Vec<Bn128Field> = vec![Field::try_from_str("42", 10).unwrap()];
 
         let hash: Bn128Field = hash_BN_128(input);
         println!("{}", hash);
 
-        assert!(hash == Field::try_from_str("12326503012965816391338144612242952408728683609716147019497703475006801258307", 10).unwrap());
+        assert!(
+            hash == Field::try_from_str(
+                "12326503012965816391338144612242952408728683609716147019497703475006801258307",
+                10
+            )
+            .unwrap()
+        );
     }
 
     #[test]
@@ -103,12 +107,18 @@ mod tests {
         let input: Vec<Bn128Field> = vec![
             Field::try_from_str("1", 10).unwrap(),
             Field::try_from_str("2", 10).unwrap(),
-        ]; 
+        ];
 
         let hash: Bn128Field = hash_BN_128(input);
         println!("{}", hash);
 
-        assert!(hash == Field::try_from_str("7853200120776062878684798364095072458815029376092732009249414926327459813530", 10).unwrap());
+        assert!(
+            hash == Field::try_from_str(
+                "7853200120776062878684798364095072458815029376092732009249414926327459813530",
+                10
+            )
+            .unwrap()
+        );
     }
 
     #[test]
@@ -117,11 +127,17 @@ mod tests {
             Field::try_from_str("1", 10).unwrap(),
             Field::try_from_str("2", 10).unwrap(),
             Field::try_from_str("3", 10).unwrap(),
-        ]; 
+        ];
 
         let hash: Bn128Field = hash_BN_128(input);
         println!("{}", hash);
 
-        assert!(hash == Field::try_from_str("6542985608222806190361240322586112750744169038454362455181422643027100751666", 10).unwrap());
+        assert!(
+            hash == Field::try_from_str(
+                "6542985608222806190361240322586112750744169038454362455181422643027100751666",
+                10
+            )
+            .unwrap()
+        );
     }
 }
